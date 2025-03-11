619ed74b5b2ad11291872ac3617c5a630a87f8d8
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2025-03-11 16:48:17 +0100

    Improve MyFeaturedImageNotice

--- admin/js/wa-golfs-editor.js
+++ admin/js/wa-golfs-editor.js
@@ -4,43 +4,51 @@
 
 // Add text after metabox
 wp.domReady(() => {
-    // Featured metabox
-    const featuredObserver = new MutationObserver((mutations, obs) => {
-        const featuredPanel = document.querySelector('.editor-post-featured-image');
-        if (featuredPanel) {
-            // Element is now available
-            addCustomText(featuredPanel, '<p class="post-description"><span class="label">INFO</span> This image will be used as a logotype</p><p><span class="important">Provide at least 1000x1000px image in transparent * .png format.</span></p>');
-            obs.disconnect(); // Stop observing once we've added our custom text
-        }
-    });
+  // Featured metabox
+  const featuredObserver = new MutationObserver((mutations, obs) => {
+    const featuredPanel = document.querySelector(".editor-post-featured-image");
+    if (featuredPanel) {
+      // Element is now available
+      addCustomText(
+        featuredPanel,
+        '<p class="post-description"><span class="label">INFO</span> This image will be used as a logotype</p><p><span class="important">Provide at least 1000x1000px image in transparent * .png format.</span></p>'
+      );
+      obs.disconnect(); // Stop observing once we've added our custom text
+    }
+  });
 
-    // Taxonomies metabox
-    const taxonomiesObserver = new MutationObserver((mutations, obs) => {
-        const taxonomiesPanel = document.querySelector('.editor-post-taxonomies__hierarchical-terms-list');
-        if (taxonomiesPanel) {
-            // Element is now available
-            addCustomText(taxonomiesPanel, '<p class="post-description"><span class="label">INFO</span> Both choose a geography & geolocation of attached farm. </p><p><span class="important">Only one geography is allowed.</span></p>');
-            obs.disconnect(); // Stop observing once we've added our custom text
-        }
-    });
-    
-    // Start observing for both sections
-    featuredObserver.observe(document.body, {
-        childList: true,
-        subtree: true
-    });
-    taxonomiesObserver.observe(document.body, {
-        childList: true,
-        subtree: true
-    });
+  // Taxonomies metabox
+  const taxonomiesObserver = new MutationObserver((mutations, obs) => {
+    const taxonomiesPanel = document.querySelector(
+      ".editor-post-taxonomies__hierarchical-terms-list"
+    );
+    if (taxonomiesPanel) {
+      // Element is now available
+      addCustomText(
+        taxonomiesPanel,
+        '<p class="post-description"><span class="label">INFO</span> Both choose a geography & geolocation of attached farm. </p><p><span class="important">Only one geography is allowed.</span></p>'
+      );
+      obs.disconnect(); // Stop observing once we've added our custom text
+    }
+  });
+
+  // Start observing for both sections
+  featuredObserver.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
+  taxonomiesObserver.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
 });
 
 function addCustomText(targetNode, html) {
-    const customText = document.createElement('div');
-    customText.innerHTML = html;
-    // customText.className = 'post-description';
-    customText.style.marginTop = '10px';
-    targetNode.parentNode.insertBefore(customText, targetNode.nextSibling);
+  const customText = document.createElement("div");
+  customText.innerHTML = html;
+  // customText.className = 'post-description';
+  customText.style.marginTop = "10px";
+  targetNode.parentNode.insertBefore(customText, targetNode.nextSibling);
 }
 
 /* 
@@ -57,44 +65,54 @@ function addCustomText(targetNode, html) {
 //     }
 // );
 
-// Display a notice if no featured image set 
-( function( wp ) {
-    var __ = wp.i18n.__;
-    var useEffect = wp.element.useEffect;
-    var useSelect = wp.data.useSelect;
-    var dispatch = wp.data.dispatch;
+// Display a notice if no featured image set for custom post types
+(function (wp) {
+  var __ = wp.i18n.__;
+  var useEffect = wp.element.useEffect;
+  var useSelect = wp.data.useSelect;
+  var dispatch = wp.data.dispatch;
 
-    function MyFeaturedImageNotice() {
-        // Unique identifier for the notice.
-        const noticeId = 'my-featured-image-notice';
+  function MyFeaturedImageNotice() {
+    // Unique identifier for the notice.
+    const noticeId = "my-featured-image-notice";
 
-        var featuredImageId = useSelect( function( select ) {
-            return select('core/editor').getEditedPostAttribute('featured_media');
-        }, [] );
+    var postType = useSelect(function (select) {
+      return select("core/editor").getCurrentPostType();
+    }, []);
 
-        useEffect(() => {
-            // If there is no featured image, display the notice.
-            if ( !featuredImageId ) {
-                dispatch( 'core/notices' ).createNotice(
-                    'warning', // Can be one of: success, info, warning, error.
-                    __( 'This post has no featured image. Consider setting one.' ), // Text string to display.
-                    {
-                        id: noticeId, // Use the unique identifier for the notice.
-                        isDismissible: true, // Whether the user can dismiss the notice.
-                        // Any actions the user can perform.
-                    }
-                );
-            } else {
-                // If a featured image is set, remove the notice.
-                dispatch( 'core/notices' ).removeNotice( noticeId );
+    var featuredImageId = useSelect(function (select) {
+      return select("core/editor").getEditedPostAttribute("featured_media");
+    }, []);
+
+    useEffect(() => {
+      // Array of custom post types
+      const customPostTypes = ["competitions", "course"];
+
+      // Check if the post type is one of the custom post types
+      if (customPostTypes.includes(postType)) {
+        // If there is no featured image, display the notice.
+        if (!featuredImageId) {
+          dispatch("core/notices").createNotice(
+            "info", // Can be one of: success, info, warning, error.
+            __("This post has no featured image. Consider setting one."), // Text string to display.
+            {
+              id: noticeId, // Use the unique identifier for the notice.
+              isDismissible: true, // Whether the user can dismiss the notice.
+              // Any actions the user can perform.
             }
-        }, [featuredImageId]); // Re-run this effect only if featuredImageId changes.
+          );
+        } else {
+          // If a featured image is set, remove the notice.
+          dispatch("core/notices").removeNotice(noticeId);
+        }
+      }
+    }, [postType, featuredImageId]); // Re-run this effect only if postType or featuredImageId changes.
 
-        // Return null because this component doesn't render anything itself.
-        return null;
-    }
+    // Return null because this component doesn't render anything itself.
+    return null;
+  }
 
-    wp.plugins.registerPlugin( 'my-featured-image-notice', {
-        render: MyFeaturedImageNotice
-    } );
-} )( window.wp );
+  wp.plugins.registerPlugin("my-featured-image-notice", {
+    render: MyFeaturedImageNotice,
+  });
+})(window.wp);
